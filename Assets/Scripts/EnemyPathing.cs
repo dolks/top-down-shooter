8d4f5057d1ed493bf6d92b81f6e6a9bf77d06ab6
Dylan Oelkers <dolks@dylans-mbp.myfiosgateway.com>
2021-01-28 23:48:18 -0500

    Refactored waypoints to use new WaveConfig script instead of having it in the EnemyPathing file

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 5f;
+    int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = Time.deltaTime * moveSpeed;
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                targetPosition,
+                movementThisFrame
+            );
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+
 }
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int numberOfEnemies;
+
+    public List<Transform> GetWaypoints()
+    {
+        var waypointsToAdd = new List<Transform>();
+        foreach (Transform waypointTransform in pathPrefab.transform)
+        {
+            waypointsToAdd.Add(waypointTransform);
+        }
+
+        return waypointsToAdd;
+    }
+
+}
