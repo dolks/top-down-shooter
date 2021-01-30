b978fa765ade9eea7abdbca2be403dc4561a705f
dolks <dylanoelkers165@gmail.com>
2021-01-30 13:07:08 -0500

    Minor update to initial value for hitpoints in Enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float hitpoints = 100;
+    [SerializeField] float hitpoints = 100f;
 
     // Start is called before the first frame update
     void Start()
