992f2ddb0b595ff9d539a8bd1da03ab018c5f112
dolks <dylanoelkers165@gmail.com>
2021-02-01 22:11:16 -0500

    Added basics of enemy firing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float hitpoints = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = -3f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        resetShotCounter();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countDownAndShoot();
+    }
+
+    private void countDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            resetShotCounter();
+        }
+    }
+
+    private void resetShotCounter()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
